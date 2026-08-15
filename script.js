66a4e36e46cdc132d46d9e3085d91db4eecf8f29
Shalini  V <105047114+shaal123@users.noreply.github.com>
2026-08-15 11:20:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+const element = document.getElementById("level");
+
+let level = 1;
+
+let parent = element.parentElement;
+
+while (parent) {
+    level++;
+    parent = parent.parentElement;
+}
+
+alert("The level of the element is: " + level);
